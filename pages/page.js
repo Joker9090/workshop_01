0144da7a5de954cf0d9f4903b84005c601c7c620
Barto <juanmartin.cerruti@gmail.com>
2023-12-06 09:36:14 -0300

    add new sections for workshops

--- pages/page.js
+++ pages/page.js
@@ -5,15 +5,50 @@ export default function Page() {
     return (
         <>
             <Head>
-                <title>Workshop -ws-3</title>
+                <title>Workshop ws-3</title>
                 <meta name="description" content="Workshop" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="">
                 <HeaderBar></HeaderBar>
+                <div className='push-headerbar'></div>
+                <SectionPinterest></SectionPinterest>
+                <SectionPinterest reversed={true}></SectionPinterest>
+                <SectionPinterest></SectionPinterest>
             </main>
         </>
+    )
+}
+
+
+export const SectionPinterest = ({ reversed }) => {
+    const obj = {
+        title: 'Hola mundo',
+        description: 'Lorem ipsum dolor sit',
+        cta: 'Ver más',
+    }
+
+    return (
+        <div className='SectionPinterest'>
+            <div className='container'>
+                <div className={`row full-height ${(reversed) ? 'reversed' : ''}`}>
+                    <div className='col-12 col-md-3 centered'>
+                        <div className='info-box'>
+                            <div className='title'><p className='mb-0'>{obj.title}</p></div>
+                            <div className='description'><p className='mb-0'>{obj.description}</p></div>
+                            <div className='cta'><p className='mb-0'>{obj.cta}</p></div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-9 relative'>
+                        <div className='img-box img-1'></div>
+                        <div className='img-box img-2'></div>
+                        <div className='img-box img-3'></div>
+                        <div className='img-box img-4'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
